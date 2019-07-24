8e36fd4a0764125dc9491f6d792f43f981e2cb77
ductran <>
2019-07-24 13:12:02 +0700

    SomeBasicTest

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const hostname = 'localhost';
+const port  = '3000';
+
+app.get('hello', (req,res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port,hostname,() => {
+        console.log('Hello, Running at', hostname ,port);
+})
